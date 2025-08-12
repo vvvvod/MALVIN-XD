88f67ffa28c7d5da6e4658bc2581751ed80f8818
vvvvod <jokerlouis509@gmail.com>
2025-08-11 19:58:45 -0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~46lGjI6D#75XxNJ1wKnooFgAcIx7Rn2q6sGx_-I0zaAWJiLM8wqU",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "50948190907", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
